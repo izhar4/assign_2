dcb32e1fe1c04bdd5a3a122e627053b674f61e35
izhar <izharmalik4@gmail.com>
2019-04-09 22:34:41 +0530

    update mongo db url

--- config/production_config.js
+++ config/production_config.js
@@ -1,2 +1,3 @@
 module.exports.APP_PORT = 8080;
-module.exports.DB_URL = 'mongodb://assign:assign123@senecaweb-shard-00-00-heyt3.mongodb.net:27017,senecaweb-shard-00-01-heyt3.mongodb.net:27017,senecaweb-shard-00-02-heyt3.mongodb.net:27017/SenecaWeb?ssl=true&replicaSet=SenecaWeb-shard-0&authSource=admin&retryWrites=true';
+// module.exports.DB_URL = 'mongodb://assign:assign123@senecaweb-shard-00-00-heyt3.mongodb.net:27017,senecaweb-shard-00-01-heyt3.mongodb.net:27017,senecaweb-shard-00-02-heyt3.mongodb.net:27017/SenecaWeb?ssl=true&replicaSet=SenecaWeb-shard-0&authSource=admin&retryWrites=true';
+module.exports.DB_URL = 'mongodb://assign:assign123@cluster0-shard-00-00-5bsrd.mongodb.net:27017,cluster0-shard-00-01-5bsrd.mongodb.net:27017,cluster0-shard-00-02-5bsrd.mongodb.net:27017/SenecaWeb?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
